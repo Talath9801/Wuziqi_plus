7b42ce622ab608e86933db5643c90e18731fc721
Talath9801 <1098454874@qq.com>
2020-07-16 20:51:14 +0800

    添加鼠标跟踪功能，但是没有效果

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -21,6 +21,12 @@ void MainWindow::paintEvent(QPaintEvent *event)
         painter.drawLine(MARGIN,MARGIN+i*CELL_SIZE,MARGIN+15*CELL_SIZE,MARGIN+i*CELL_SIZE);
         painter.drawLine(MARGIN+i*CELL_SIZE,MARGIN,MARGIN+i*CELL_SIZE,MARGIN+15*CELL_SIZE);
     }//绘制棋盘
+
+    painter.setBrush(Qt::black);
+    if(ifClick)
+    {
+        painter.drawEllipse(toClick.x(),toClick.y(),MARK_SIZE,MARK_SIZE);
+    }
 }
 
 MainWindow::~MainWindow()
@@ -33,5 +39,19 @@ void MainWindow::inGame()//构造函数中调用，初始化一个游戏
     mygame=new Game;
     mygame->myGameCondition=playing;//设置为游戏中
     mygame->startGame();
+    update();
+}
+void MainWindow::mouseTracking(QMouseEvent *event)
+{
+    QPoint mypoint=event->pos();
+    for(int i=0;i<16*16;i++)
+    {
+        if(inRange(mypoint,mygame->pointList[i],CLICK_RANGE))//遍历pointlist，看是否有与鼠标位置的距离小于识别距离的点位
+        {
+            toClick=mygame->pointList[i];
+            ifClick=true;
+            break;
+        }
+    }
     update();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QMouseEvent>
 #include "game.h"
 #include "distance.h"
 
@@ -17,13 +18,15 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
     void paintEvent(QPaintEvent *event);
-
+    void mouseTracking(QMouseEvent *event);
     void inGame();//进入游戏
 
 
 private:
     Ui::MainWindow *ui;
     Game *mygame;
+    QPoint toClick;
+    bool ifClick=false;
 };
 
 #endif // MAINWINDOW_H
